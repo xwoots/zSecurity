9d6d32a5822bd3ee1724ee3ad3df18632e91e4bc
xwoot <57961128+xwoot@users.noreply.github.com>
2022-06-26 13:00:08 +0200

    Create auto-update-route53.sh

---
+++ auto-update-route53.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Extract information about the Instance
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id/)
+AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4/)
+
+# Extract tags associated with instance
+ZONE_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_ZONE`].Value' --output text)
+NAME_TAG=$(aws ec2 describe-tags --region ${AZ::-1} --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[?Key==`AUTO_DNS_NAME`].Value' --output text)
+
+# Update Route 53 Record Set based on the Name tag to the current Public IP address of the Instance
+aws route53 change-resource-record-sets --hosted-zone-id $ZONE_TAG --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$NAME_TAG'","Type":"A","TTL":300,"ResourceRecords":[{"Value":"'$MY_IP'"}]}}]}'
